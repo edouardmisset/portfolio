b6edc7e01aa1a2265d18542cac84111356db06c8
Edouard Misset <edouardmisset@gmail.com>
2021-04-01 21:32:37 +0200

    grabbed the code for the burger menu for 30 seconds. Needs implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,36 @@ export default function Header() {
     <header>
       <nav>
         <ul>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
+          <li className="link">
+            <a href="#about">About</a>
+          </li>
+          <li className="link">
+            <a href="#skills">Skills</a>
+          </li>
+          <li className="link">
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li className="link">
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </nav>
     </header>
   );
 }
+
+/* 
+<header class="container">
+      <input type="checkbox" name="menu" id="menu" class="menu-toggler" />
+      <label for="menu" class="menu-toggler-label">
+        <i data-feather="menu"> </i>
+      </label>
+      <ul class="stagger-menu hover-nav">
+        <li style="--i: 0"><a href="#">Home</a></li>
+        <li style="--i: 1"><a href="#">Pricing</a></li>
+        <li style="--i: 2"><a href="#">Account</a></li>
+        <li style="--i: 3"><a href="#">Support</a></li>
+        <li style="--i: 4"><a href="#">About</a></li>
+      </ul>
+    </header>
+*/
