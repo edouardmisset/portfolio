43f7e919108363ef2e116e5e6ca4bd5e1de0c6dd
Edouard Misset <edouardmisset@gmail.com>
2021-04-01 21:16:07 +0200

    added the footer to the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
