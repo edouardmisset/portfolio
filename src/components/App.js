48dd9bf38fb48ba573d7ad15d58b738351088695
Edouard Misset <edouardmisset@gmail.com>
2021-04-01 21:13:58 +0200

    Created Main

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Main from './Main';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import './Main.css';
+
+export default function Main() {
+  return (
+    <main>
+      <section className="section" id="about">
+        About
+      </section>
+      <section className="section" id="skills">
+        Skills
+      </section>
+      <section className="section" id="portfolio">
+        Portfolio
+      </section>
+      <section className="section" id="contact">
+        Contact
+      </section>
+    </main>
+  );
+}
