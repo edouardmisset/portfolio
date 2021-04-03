fc617227ba136a6193309caf407e45b0be6039d1
Edouard Misset <edouardmisset@gmail.com>
2021-04-03 16:43:16 +0200

    started building the grid for my portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,6 +6,17 @@ export default function Portfolio() {
       <h2 className="section-title" id="portfolio">
         Portfolio
       </h2>
+      <div className="site-container">
+        <div className="site-preview">1</div>
+        <div className="site-preview">2</div>
+        <div className="site-preview">3</div>
+        <div className="site-preview">4</div>
+        <div className="site-preview">5</div>
+        <div className="site-preview">6</div>
+        <div className="site-preview">7</div>
+        <div className="site-preview">8</div>
+        <div className="site-preview">9</div>
+      </div>
     </section>
   );
 }
