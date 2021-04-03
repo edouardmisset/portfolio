28026411c714360f6df6db3c96d71f7a0a3efc24
Edouard Misset <edouardmisset@gmail.com>
2021-04-03 16:16:48 +0200

    Started on the skills' page, need to add the gauge and the icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,6 +6,23 @@ export default function Skills() {
       <h2 className="section-title" id="skills">
         Skills
       </h2>
+      <h3 className="list-title">Front-End</h3>
+      <ul className="list-skills">
+        {/* Add icons to the language's name */}
+        {/* Add gauge to the language */}
+        <li className="skill-item">HTML</li>
+        <li className="skill-item">CSS</li>
+        <li className="skill-item">JS</li>
+        <li className="skill-item">React</li>
+      </ul>
+      <h3 className="list-title">Back-End</h3>
+      <ul className="list-skills">
+        {/* Add icons to the language's name */}
+        {/* Add gauge to the language */}
+        <li className="skill-item">NodeJS</li>
+        <li className="skill-item">Express</li>
+        <li className="skill-item">SQL</li>
+      </ul>
     </section>
   );
 }
