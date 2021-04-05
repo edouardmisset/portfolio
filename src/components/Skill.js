eb4daf4a01a3458be84ddfa57e04b28015707ca1
Edouard Misset <edouardmisset@gmail.com>
2021-04-05 16:27:54 +0200

    added title to images

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -4,7 +4,7 @@ export default function Skill({ name, level, color, icon }) {
   return (
     <span className="skill" id={name}>
       <h5 className="language">
-        <img src={icon} width="32" height="32" alt={name} />
+        <img src={icon} width="32" height="32" alt={name} title={name} />
         {name}
       </h5>
       <div className="gauge">
