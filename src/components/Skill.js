647fc957df2db4a7efc3de44ee8ef605d75f50fd
Edouard Misset <edouardmisset@gmail.com>
2021-04-05 11:13:29 +0200

    Added languages colors to the component & cleanup

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,11 +1,17 @@
 import './Skill.css';
 
-export default function Skill({ name, level }) {
+export default function Skill({ name, level, color }) {
   return (
     <span className="skill" id={name}>
       <h5 className="language">{name}</h5>
       <div className="gauge">
-        <div className={level} style={{ width: `${parseInt(level, 10)}%` }} />
+        <div
+          className={level}
+          style={{
+            width: `${parseInt(level, 10)}%`,
+            backgroundColor: `${color}`,
+          }}
+        />
       </div>
     </span>
   );
