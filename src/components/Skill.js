0b2c431b0853e9a8a54264cc717c06dbdcd449e5
Edouard Misset <edouardmisset@gmail.com>
2021-04-05 16:27:15 +0200

    added icons to the language's name

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,9 +1,12 @@
 import './Skill.css';
 
-export default function Skill({ name, level, color }) {
+export default function Skill({ name, level, color, icon }) {
   return (
     <span className="skill" id={name}>
-      <h5 className="language">{name}</h5>
+      <h5 className="language">
+        <img src={icon} width="32" height="32" alt={name} />
+        {name}
+      </h5>
       <div className="gauge">
         <div
           className={level}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,12 @@
 import './Skills.css';
 import Skill from './Skill';
+import htmlIcon from '../assets/icons/HTML-icon.png';
+import cssIcon from '../assets/icons/CSS-icon.png';
+import jsIcon from '../assets/icons/JS-icon.png';
+import reactIcon from '../assets/icons/React-icon.png';
+import nodeJSIcon from '../assets/icons/nodeJS-icon.png';
+import expressJSIcon from '../assets/icons/expressJS-icon.png';
+import MySQLIcon from '../assets/icons/MySQL-icon.png';
 
 export default function Skills() {
   return (
@@ -9,33 +16,49 @@ export default function Skills() {
       </h2>
       <h3 className="list-title">Front-End</h3>
       <ul className="list-skills">
-        {/* Add icons to the language's name */}
-        {/* Add gauge to the language */}
         <li className="skill-item">
-          <Skill name="HTML" level="90" color="var(--html)" />
+          <Skill name="HTML" level="90" color="var(--html)" icon={htmlIcon} />
         </li>
         <li className="skill-item">
-          <Skill name="CSS" level="80" color="var(--css)" />
+          <Skill name="CSS" level="80" color="var(--css)" icon={cssIcon} />
         </li>
         <li className="skill-item">
-          <Skill name="JS" level="70" color="var(--javascript)" />
+          <Skill name="JS" level="70" color="var(--javascript)" icon={jsIcon} />
         </li>
         <li className="skill-item">
-          <Skill name="React" level="60" color="var(--react)" />
+          <Skill
+            name="React"
+            level="60"
+            color="var(--react)"
+            icon={reactIcon}
+          />
         </li>
       </ul>
       <h3 className="list-title">Back-End</h3>
       <ul className="list-skills">
-        {/* Add icons to the language's name */}
-        {/* Add gauge to the language */}
         <li className="skill-item">
-          <Skill name="NodeJS" level="30" color="var(--nodeJS)" />
+          <Skill
+            name="NodeJS"
+            level="30"
+            color="var(--nodeJS)"
+            icon={nodeJSIcon}
+          />
         </li>
         <li className="skill-item">
-          <Skill name="Express" level="30" color="var(--express)" />
+          <Skill
+            name="Express"
+            level="30"
+            color="var(--express)"
+            icon={expressJSIcon}
+          />
         </li>
         <li className="skill-item">
-          <Skill name="MySQL" level="40" color="var(--mysql)" />
+          <Skill
+            name="MySQL"
+            level="40"
+            color="var(--mysql)"
+            icon={MySQLIcon}
+          />
         </li>
       </ul>
     </section>
