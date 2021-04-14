82dd1fbf11e9f030f8378bff7c23679e59b70c7a
Edouard Misset <edouardmisset@gmail.com>
2021-04-14 18:07:24 +0200

    still needs work for the progress bar

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import './Progress.css';
 
+// const window = document.querySelector('window');
+
 export default function Progress() {
+  const [viewportHeight, setViewportHeight] = useState(0);
+
+  // Get the viewport height when component loads
+  useEffect(() => {
+    const { body } = document;
+    const html = document.documentElement;
+    const docHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+    setViewportHeight(document.documentElement.clientHeight);
+    console.log(viewportHeight, docHeight);
+  }, []);
+
+  function handleProgressUpdate() {
+    console.log(viewportHeight);
+  }
+
   return (
     <>
-      <progress value="0" />
+      <progress value="0" onScroll={handleProgressUpdate} />
     </>
   );
 }
