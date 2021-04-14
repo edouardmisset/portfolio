eee77e85420ea0dbbf40fa4ce1eb24fe8f5c0be3
Edouard Misset <edouardmisset@gmail.com>
2021-04-14 14:54:17 +0200

    added the progress component to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import Progress from './Progress';
 
 export default function App() {
   return (
     <>
+      <Progress />
       <Header />
       <Main />
       <Footer />
