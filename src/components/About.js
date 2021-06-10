82626cbf3fe4bf866c8183ebb79bf51e4d0fff74
Edouard Misset <edouardmisset@gmail.com>
2021-06-10 16:03:41 +0200

    Styled the about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,12 @@ import './About.css';
 export default function About() {
   return (
     <section id="about">
-      <h1 id="about-me">
-        Hi there! <br /> I'm{' '}
-        <span id="my-name" className="fancy">
+      <h1 className="title" id="about-me">
+        Hi there! 👋
+        <br /> I'm{' '}
+        <strong id="my-name" className="fancy">
           Edouard
-        </span>{' '}
+        </strong>{' '}
         a full-stack developer!
       </h1>
       <p id="tag-line">Let's build something different together</p>
